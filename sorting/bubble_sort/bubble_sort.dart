ca29a568f084cf55a21a3526c3cae8dffab2bade
sunnydodti <sunnydodti@gmail.com>
2024-05-15 16:32:22 +0530

    bubble sort - dart

---
+++ sorting/bubble_sort/bubble_sort.dart
@@ -0,0 +1,35 @@
+List<int> bubbleSort(List<int> elements) {
+  for (int i = 0; i < elements.length; i++) {
+    for (int j = 0; j < elements.length - 1; j++) {
+      if (elements[j] > elements[j + 1]) {
+        int temp = elements[j];
+        elements[j] = elements[j + 1];
+        elements[j + 1] = temp;
+      }
+    }
+  }
+  return elements;
+}
+
+List<int> bubbleSortOptimized(List<int> elements) {
+  int temp = 0;
+  int length = elements.length;
+
+  for (int i = 0; i < length; i++) {
+    for (int j = 0; j < length - 1 - i; j++) {
+      if (elements[j] > elements[j + 1]) {
+        temp = elements[j];
+        elements[j] = elements[j + 1];
+        elements[j + 1] = temp;
+      }
+    }
+  }
+  return elements;
+}
+
+void main() {
+  List<int> input = [3, 19, 8, 2, 5, 1];
+  print(input);
+  print("bubbleSort ${bubbleSort(List<int>.from(input))}");
+  print("bubbleSortOptimized ${bubbleSortOptimized(List<int>.from(input))}");
+}
